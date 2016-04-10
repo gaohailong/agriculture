50570a98e1e90480a28560b5d0d54ac6c61bb3db
gaohailong <190859080@qq.com>
2016-04-10 15:45:51 +0800

    问答界面的item

--- app/src/main/java/com/sxau/agriculture/view/fragment/MessageFragment.java
+++ app/src/main/java/com/sxau/agriculture/view/fragment/MessageFragment.java
@@ -1,6 +1,8 @@
 package com.sxau.agriculture.view.fragment;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,21 +10,30 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.sxau.agriculture.agriculture.R;
+import com.sxau.agriculture.view.activity.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class MessageFragment extends Fragment {
-
+    private MainActivity mainActivity;
 
     public MessageFragment() {
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mainActivity = (MainActivity) activity;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+        return view;
     }
-
 }
