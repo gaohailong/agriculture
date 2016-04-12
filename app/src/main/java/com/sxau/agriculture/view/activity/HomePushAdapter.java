2c28cacc0125f38a36e28e58b5711e546a89223e
silence-crystal <529832605@qq.com>
2016-04-12 21:59:42 +0800

    首页

---
+++ app/src/main/java/com/sxau/agriculture/view/activity/HomePushAdapter.java
@@ -0,0 +1,72 @@
+package com.sxau.agriculture.view.activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sxau.agriculture.agriculture.R;
+import com.sxau.agriculture.view.fragment.PushBean;
+
+/**
+ * Created by czz on 2016/4/8.
+ */
+public class HomePushAdapter extends BaseAdapter{
+    PushBean data[];
+    private Context context;
+
+    public HomePushAdapter(PushBean[] data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return data.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+    public class ViewHolder{
+        ImageView iv_news;
+        TextView tv_title;
+        TextView tv_time;
+        ImageView iv_browse;
+        TextView tv_read;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder=new ViewHolder();
+
+        if (convertView==null){
+            convertView= LayoutInflater.from(context).inflate(R.layout.homeitem_listview,null);
+            holder.iv_news= (ImageView) convertView.findViewById(R.id.iv_news);
+            holder.tv_title= (TextView) convertView.findViewById(R.id.tv_title);
+            holder.tv_time= (TextView) convertView.findViewById(R.id.tv_time);
+            holder.iv_browse= (ImageView) convertView.findViewById(R.id.iv_browse);
+            holder.tv_read= (TextView) convertView.findViewById(R.id.tv_read);
+            convertView.setTag(holder);
+        }else{
+            holder= (ViewHolder) convertView.getTag();
+        }
+        PushBean pushBean=data[position];
+        holder.iv_news.setImageResource(R.drawable.dialsmall);
+        holder.tv_title.setText(pushBean.getTitle());
+        holder.tv_time.setText(pushBean.getTime());
+        holder.iv_browse.setImageResource(R.drawable.browse);
+        holder.tv_read.setText(pushBean.getRead());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/sxau/agriculture/view/fragment/HomeFragment.java
+++ app/src/main/java/com/sxau/agriculture/view/fragment/HomeFragment.java
@@ -1,16 +1,19 @@
 package com.sxau.agriculture.view.fragment;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.sxau.agriculture.agriculture.R;
+import com.sxau.agriculture.view.activity.HomePushAdapter;
 
 public class HomeFragment extends BaseFragment {
+
+
+    private Context mContext;
 
 /*
 
@@ -24,19 +27,70 @@ public class HomeFragment extends BaseFragment {
         return fragment;
     }*/
 
-   /* @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       *//* if (getArguments() != null) {
+    /* @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+        *//* if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }*//*
     }*/
+    private ListView lv_push;
+    private View mView;
+    HomePushAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        lv_push = (ListView) mView.findViewById(R.id.lv_push);
+
+        PushBean[] pushs=new PushBean[6];
+        pushs[0]=new PushBean();
+        pushs[0].setRead("12万");
+        pushs[0].setBrowseid(R.drawable.browse);
+        pushs[0].setNewsid(R.drawable.dialsmall);
+        pushs[0].setTime("2016.4.9");
+        pushs[0].setTitle("全面推动“三品一标”工作再上新台阶");
+
+        pushs[1]=new PushBean();
+        pushs[1].setRead("12万");
+        pushs[1].setBrowseid(R.drawable.browse);
+        pushs[1].setNewsid(R.drawable.dialsmall);
+        pushs[1].setTime("2016.4.9");
+        pushs[1].setTitle("全面推动“三品一标”工作啦啦啦再上新台阶");
+
+        pushs[2]=new PushBean();
+        pushs[2].setRead("12万");
+        pushs[2].setBrowseid(R.drawable.browse);
+        pushs[2].setNewsid(R.drawable.dialsmall);
+        pushs[2].setTime("2016.4.9");
+        pushs[2].setTitle("帅子是煞笔");
+
+        pushs[3]=new PushBean();
+        pushs[3].setRead("12万");
+        pushs[3].setBrowseid(R.drawable.browse);
+        pushs[3].setNewsid(R.drawable.dialsmall);
+        pushs[3].setTime("2016.4.9");
+        pushs[3].setTitle("全面推动“三品一标”工作再上新台阶");
+
+        pushs[4]=new PushBean();
+        pushs[4].setRead("12万");
+        pushs[4].setBrowseid(R.drawable.browse);
+        pushs[4].setNewsid(R.drawable.dialsmall);
+        pushs[4].setTime("2016.4.9");
+        pushs[4].setTitle("全面推动“三品一标”工作再上新台阶");
+
+        pushs[5]=new PushBean();
+        pushs[5].setRead("12万");
+        pushs[5].setBrowseid(R.drawable.browse);
+        pushs[5].setNewsid(R.drawable.dialsmall);
+        pushs[5].setTime("2016.4.9");
+        pushs[5].setTitle("全面推动“三品一标”工作再上新台阶");
+
+        adapter=new HomePushAdapter(pushs,HomeFragment.this.getActivity());
+        lv_push.setAdapter(adapter);
+        return mView;
     }
 
 }
