a23e240de297b53af1c8b4838a2fec4b45b802a2
gaohailong <190859080@qq.com>
2016-04-13 16:30:42 +0800

    修改部分代码格式

--- app/src/main/java/com/sxau/agriculture/view/fragment/InfoFragment.java
+++ app/src/main/java/com/sxau/agriculture/view/fragment/InfoFragment.java
@@ -1,32 +1,22 @@
 package com.sxau.agriculture.view.fragment;
 
 
-import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
-import com.ogaclejapan.smarttablayout.utils.ViewPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import com.sxau.agriculture.agriculture.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class InfoFragment extends Fragment {
-    private  View mview;
-
+public class InfoFragment extends BaseFragment {
+    private View mview;
     private ViewPager viewpager;
-//    private ImageButton ibHead;
+
+    //    private ImageButton ibHead;
 //    private TextView name;
 //    private TextView date;
 //    private TextView distance;
@@ -34,28 +24,19 @@ public class InfoFragment extends Fragment {
 //    private TextView content;
     public InfoFragment() {
     }
-@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
 
-        mview=inflater.inflate(R.layout.fragment_info, container, false);
-
-
-        viewpager= (ViewPager) mview.findViewById(R.id.vp_infoSupplyOrDemand);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mview = inflater.inflate(R.layout.fragment_info, container, false);
+        viewpager = (ViewPager) mview.findViewById(R.id.vp_infoSupplyOrDemand);
         FragmentPagerItemAdapter fragmentadapter = new FragmentPagerItemAdapter(getChildFragmentManager()
-            , FragmentPagerItems.with(getContext())
-            .add("供应",InfoListViewFragment.class)
-            .add("需求", InfoListViewFragment.class)
-
-            .create());
-
-
-    viewpager.setAdapter(fragmentadapter);
-    SmartTabLayout viewPagerTab = (SmartTabLayout) mview.findViewById(R.id.viewpagertab);
-    viewPagerTab.setViewPager(viewpager);
-    return mview;
-
+                , FragmentPagerItems.with(getContext())
+                .add("供应", InfoListViewFragment.class)
+                .add("需求", InfoListViewFragment.class)
+                .create());
+        viewpager.setAdapter(fragmentadapter);
+        SmartTabLayout viewPagerTab = (SmartTabLayout) mview.findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewpager);
+        return mview;
     }
-
-    
 }
