f4e4422052fe179d0a881fda507ae0fab6fe1d7a
Yawen_Li <243845305@qq.com>
2016-05-05 18:18:46 +0800

    增加了全局获取context的方式，在agriculture中

--- app/src/main/java/com/sxau/agriculture/AgricultureApplication.java
+++ app/src/main/java/com/sxau/agriculture/AgricultureApplication.java
@@ -1,9 +1,21 @@
 package com.sxau.agriculture;
 
 import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by gaohailong on 2016/4/6.
  */
 public class AgricultureApplication extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext(){
+        return context;
+    }
 }
