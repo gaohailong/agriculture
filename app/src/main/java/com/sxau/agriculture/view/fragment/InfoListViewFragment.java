7fd6c4fccc2c2fdf12a836e4c8563a02b495ff0c
tianshuai <564241248@qq.com>
2016-05-08 11:06:50 +0800

    修改信息界面

--- app/src/main/java/com/sxau/agriculture/view/fragment/InfoListViewFragment.java
+++ app/src/main/java/com/sxau/agriculture/view/fragment/InfoListViewFragment.java
@@ -24,6 +24,9 @@ import com.sxau.agriculture.presenter.fragment_presenter.InfoListViewPresenter;
 import com.sxau.agriculture.presenter.fragment_presenter_interface.IInfoListViewPresenter;
 import com.sxau.agriculture.view.fragment_interface.IInfoListViewFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 信息专区ListView
  * @author 田帅
@@ -31,10 +34,12 @@ import com.sxau.agriculture.view.fragment_interface.IInfoListViewFragment;
 
 
 
-public class InfoListViewFragment extends BaseFragment implements IInfoListViewFragment,AdapterView.OnItemClickListener {
+public class InfoListViewFragment extends BaseFragment implements IInfoListViewFragment,AdapterView.OnItemClickListener{
         private  View mview;
         private ListView lv_Info;
         private ImageView iv_collection;
+        private InfoData minfoData;
+        private List<InfoData> infoDatas=new ArrayList<InfoData>();
 
         private IInfoListViewPresenter iInfoListViewPresenter;
 
@@ -51,78 +56,27 @@ public class InfoListViewFragment extends BaseFragment implements IInfoListViewF
                 //将InfoLvFragment与InfolvPresenter绑定
                 iInfoListViewPresenter = new InfoListViewPresenter(InfoListViewFragment.this);
 
-
                 mview=inflater.inflate(R.layout.fragment_info_listview, container, false);
 
                 lv_Info= (ListView) mview.findViewById(R.id.lv_info);
                 iv_collection= (ImageView) mview.findViewById(R.id.iv_demand_collection);
 
 
-                InfoData[] infoDatas=new InfoData[5];
-
-                infoDatas[0]=new InfoData();
-                infoDatas[0].setIvHead(R.mipmap.img_default_user_portrait_150px);
-                infoDatas[0].setName("天和集团");
-                infoDatas[0].setDate("2013.06.22");
-                infoDatas[0].setDistance("山西");
-                infoDatas[0].setTitle("收购苹果两万斤");
-                infoDatas[0].setIvLocation(R.drawable.ic_location_48dp);
-                infoDatas[0].setLvCollection(R.drawable.ic_collect_having_48dp);
-                infoDatas[0].setContent("对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于");
-
-                infoDatas[1]=new InfoData();
-                infoDatas[1].setIvHead(R.mipmap.img_default_user_portrait_150px);
-                infoDatas[1].setName("天和集团");
-                infoDatas[1].setDate("2013.06.22");
-                infoDatas[1].setDistance("山西");
-                infoDatas[1].setTitle("收购苹果两万斤");
-                infoDatas[1].setLvCollection(R.drawable.ic_collect_nothave_48dp);
-                infoDatas[1].setIvLocation(R.drawable.ic_location_48dp);
-                infoDatas[1].setContent("对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于");
-
-                infoDatas[2]=new InfoData();
-                infoDatas[2].setIvHead(R.mipmap.img_default_user_portrait_150px);
-                infoDatas[2].setName("天和集团");
-                infoDatas[2].setDate("2013.06.22");
-                infoDatas[2].setDistance("山西");
-                infoDatas[2].setTitle("收购苹果两万斤收购苹果两万斤收购苹果两万斤收购苹果两万斤收购苹果两万斤");
-                infoDatas[2].setLvCollection(R.drawable.ic_collect_have_48dp);
-                infoDatas[2].setIvLocation(R.drawable.ic_location_48dp);
-                infoDatas[2].setContent("对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于");
-
-                infoDatas[3]=new InfoData();
-                infoDatas[3].setLvCollection(R.drawable.ic_collect_have_48dp);
-                infoDatas[3].setIvHead(R.mipmap.img_default_user_portrait_150px);
-                infoDatas[3].setName("天和集团");
-                infoDatas[3].setDate("2013.06.22");
-                infoDatas[3].setDistance("山西");
-                infoDatas[3].setTitle("收购苹果两万斤");
-                infoDatas[3].setIvLocation(R.drawable.ic_location_48dp);
-                infoDatas[3].setContent("对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于");
-
-                infoDatas[4]=new InfoData();
-                infoDatas[4].setLvCollection(R.drawable.ic_collect_have_48dp);
-                infoDatas[4].setIvHead(R.mipmap.img_default_user_portrait_150px);
-                infoDatas[4].setName("天和集团");
-                infoDatas[4].setDate("2013.06.22");
-                infoDatas[4].setDistance("山西");
-                infoDatas[4].setIvLocation(R.drawable.ic_location_48dp);
-                infoDatas[4].setTitle("收购苹果两万斤");
-                infoDatas[4].setContent("对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于");
-
-
+                initInfoData();
                 BaseAdapter adapter=new InfoDemandAdapter(InfoListViewFragment.this.getActivity(),infoDatas);
                 lv_Info.setAdapter(adapter);
+                lv_Info.setOnItemClickListener(this);
                 return mview;
         }
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(InfoListViewFragment.this.getActivity(), InfoContentActivity.class);
-        startActivity(intent);
-//        Toast.makeText(InfoListViewFragment.this.getActivity(),"position:"+position+"id:"+id,Toast.LENGTH_SHORT).show();
-//       InfoSupplyFragment infoSupplyFragment=new InfoSupplyFragment();
-//        getFragmentManager().beginTransaction().replace(R.id.,infoSupplyFragment).commit();
-    }
+        public void initInfoData(){
+                InfoData infoData=new InfoData(R.drawable.ic_collect_have_48dp,R.mipmap.img_default_user_portrait_150px,"天和集团","2013.06.22","2千米","收购苹果两万斤","对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于对于",R.drawable.ic_location_48dp);
+                infoDatas.add(infoData);
+                infoDatas.add(infoData);
+                infoDatas.add(infoData);
+                infoDatas.add(infoData);
+                infoDatas.add(infoData);
+        }
+
 //------------------接口方法-------------------
         @Override
         public void updateView() {
@@ -143,5 +97,14 @@ public class InfoListViewFragment extends BaseFragment implements IInfoListViewF
         public int getCollectState() {
                 return 0;
         }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent=new Intent(InfoListViewFragment.this.getActivity(),InfoContentActivity.class);
+                startActivity(intent);
+        }
+
+
 //----------------接口方法结束-------------------
 }
