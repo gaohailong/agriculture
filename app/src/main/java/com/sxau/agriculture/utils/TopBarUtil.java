d03504be0abf755726d68a774a4581e5751c234b
gaohailong <190859080@qq.com>
2016-05-08 21:37:09 +0800

    顶部标题栏修改

--- app/src/main/java/com/sxau/agriculture/utils/TopBarUtil.java
+++ app/src/main/java/com/sxau/agriculture/utils/TopBarUtil.java
@@ -3,6 +3,9 @@ package com.sxau.agriculture.utils;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -13,6 +16,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 import com.sxau.agriculture.agriculture.R;
 import com.sxau.agriculture.widgets.RoundImageView;
 
@@ -142,8 +146,8 @@ public class TopBarUtil extends RelativeLayout {
         rightImageView.setVisibility(View.GONE);
         //TODO 将图片换做网络图片
        // Bitmap leftRoundBitmapImage= BitmapUtil.decodedBitmapFromResource(getResources(),R.mipmap.img_default_user_portrait_150px,45,45);
-        Picasso.with(context).load(R.mipmap.img_default_user_portrait_150px)
-                .placeholder(R.mipmap.img_default_user_portrait_150px).resize(50, 50).centerCrop().into(leftRoundImage);
+        Picasso.with(context).load(R.mipmap.img_default_user_portrait_150px).transform(new PicassoCirclTransform())
+                .placeholder(R.mipmap.img_default_user_portrait_150px).resize(100, 100).centerCrop().into(leftImageView);
     }
 
     //设置左边圆角图片是否可见
@@ -224,6 +228,38 @@ public class TopBarUtil extends RelativeLayout {
             rightImageView.setImageResource(id);
         } else {
             rightImageView.setVisibility(View.GONE);
+        }
+    }
+
+
+    public class PicassoCirclTransform implements Transformation {
+        @Override
+        public Bitmap transform(Bitmap source) {
+            int size = Math.min(source.getWidth(), source.getHeight());
+
+            int x = (source.getWidth() - size) / 2;
+            int y = (source.getHeight() - size) / 2;
+
+            Bitmap squaredBitmap = Bitmap
+                    .createBitmap(source, x, y, size, size);
+            if (squaredBitmap != source) {
+                source.recycle();
+            }
+            Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            BitmapShader shader = new BitmapShader(squaredBitmap,
+                    BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+            paint.setShader(shader);
+            paint.setAntiAlias(true);
+            float r = size / 2f;
+            canvas.drawCircle(r, r, r, paint);
+            squaredBitmap.recycle();
+            return bitmap;
+        }
+        @Override
+        public String key() {
+            return "circle";
         }
     }
 }
