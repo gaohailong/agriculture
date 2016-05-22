d43c50487c91d13665645431f9c8c0359a4bf3a7
tianshuai <564241248@qq.com>
2016-05-22 13:26:17 +0800

    增加发布供求界面

--- app/src/main/java/com/sxau/agriculture/view/activity/InfoReleaseActivity.java
+++ app/src/main/java/com/sxau/agriculture/view/activity/InfoReleaseActivity.java
@@ -1,16 +1,24 @@
 package com.sxau.agriculture.view.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.sxau.agriculture.agriculture.R;
 
 /**
- * Created by Administrator on 2016/5/19.
+ * Info发布供求界面
+ * @author 田帅.
  */
 public class InfoReleaseActivity extends BaseActivity{
+    private ImageView ivPhoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_release);
+        initView();
+
+    }
+    public void initView(){
+        ivPhoto= (ImageView) findViewById(R.id.iv_info_release_photo);
     }
 }
