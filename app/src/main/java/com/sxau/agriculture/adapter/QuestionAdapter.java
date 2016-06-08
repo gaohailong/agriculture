c5f48a3761f3da049ee2445c2192a2429fe414d6
silence-crystal <529832605@qq.com>
2016-06-08 16:06:38 +0800

    修改问题列表item的布局

--- app/src/main/java/com/sxau/agriculture/adapter/QuestionAdapter.java
+++ app/src/main/java/com/sxau/agriculture/adapter/QuestionAdapter.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 import com.sxau.agriculture.agriculture.R;
 import com.sxau.agriculture.bean.QuestionData;
+import com.sxau.agriculture.utils.TimeUtil;
 
 import java.util.ArrayList;
 
@@ -53,33 +54,27 @@ public class QuestionAdapter extends BaseAdapter implements View.OnClickListener
         if (convertView==null){
             convertView=LayoutInflater.from(context).inflate(R.layout.item_question_list, null);
             holder = new ViewHolder();
-            holder.rv_head = (ImageView) convertView.findViewById(R.id.rv_head);
             holder.iv_fav = (ImageView) convertView.findViewById(R.id.iv_fav);
-            holder.tv_name = (TextView) convertView.findViewById(R.id.tv_name);
             holder.tv_title = (TextView) convertView.findViewById(R.id.tv_title);
-            holder.tv_content = (TextView) convertView.findViewById(R.id.tv_content);
-            holder.ll_answer = (LinearLayout) convertView.findViewById(R.id.ll_answer);
-            holder.ll_fav_background  = (LinearLayout) convertView.findViewById(R.id.ll_fav_background);
+            holder.tv_answer = (TextView) convertView.findViewById(R.id.tv_answer);
             holder.v_left = convertView.findViewById(R.id.v_left);
+            holder.tv_ntdmix= (TextView) convertView.findViewById(R.id.tv_ntdmix);
             convertView.setTag(holder);
         }else {
             holder = (ViewHolder) convertView.getTag();
         }
         QuestionData questionData = questionDatas.get(position);
-        Picasso.with(context).load(R.mipmap.img_default_user_portrait_150px).resize(150,150).centerCrop().placeholder(R.mipmap.img_default_user_portrait_150px)
-                .error(R.mipmap.img_default_user_portrait_150px).into(holder.rv_head);
-//        holder.tv_name.setText(questionData.getUser().getName());
         holder.tv_title.setText(questionData.getTitle());
+        holder.tv_ntdmix.setText(questionData.getUser().getName()+"提问于"+ TimeUtil.format(questionData.getWhenCreated()));
         if(questionData.getTitle()!=null && !questionData.getQuestionAuditState().equals("WAIT_AUDITED")
                 && !questionData.getQuestionResolveState().equals("WAIT_RESOLVE")){
-        holder.tv_content.setText(questionData.getAnswers().toString());
+        holder.tv_answer.setText(questionData.getAnswers().toString());
         holder.v_left.setBackgroundColor(Color.parseColor("#009688"));
         }else {
-            holder.ll_answer.setVisibility(View.GONE);
+            holder.tv_answer.setVisibility(View.GONE);
         }
 
         holder.iv_fav.setOnClickListener(this);
-        holder.ll_fav_background.setOnClickListener(this);
        // holder.ll_SubjectSkip.setOnClickListener(this);
 
         return convertView;
@@ -92,10 +87,10 @@ public class QuestionAdapter extends BaseAdapter implements View.OnClickListener
         switch (v.getId()){
             case R.id.iv_fav:
                 if (favIndex==0){
-                    holder.iv_fav.setImageResource(R.drawable.ic_praise_48px);
+                    holder.iv_fav.setImageResource(R.drawable.collection_fill);
                     favIndex=1;
                 }else {
-                    holder.iv_fav.setImageResource(R.drawable.ic_no_praise_48px);
+                    holder.iv_fav.setImageResource(R.drawable.collection);
                     favIndex=0;
                 }
                 break;
@@ -106,14 +101,11 @@ public class QuestionAdapter extends BaseAdapter implements View.OnClickListener
     }
 
     private class ViewHolder{
-        private ImageView rv_head;
         private ImageView iv_fav;
 
-        private TextView tv_name;
+        private TextView tv_ntdmix;
         private TextView tv_title;
-        private TextView tv_content;
-        private LinearLayout ll_answer;
-        private LinearLayout ll_fav_background;//收藏的linearlayout
+        private TextView tv_answer;
         private View v_left;
 
     }
